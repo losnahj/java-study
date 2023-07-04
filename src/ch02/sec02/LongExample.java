e46c6d56650a3df89452524b11ba4dd30ba02746
DESKTOP-CSH3D9L\My <zmfpdlwm130@naver.com>
2023-07-04 21:23:30 +0900

    13강) 2.1 변수 선언

---
+++ src/ch02/sec02/LongExample.java
@@ -0,0 +1,13 @@
+package ch02.sec02;
+
+public class LongExample {
+    public static void main(String[] args) {
+        long a = 10;
+        long b = 10;
+
+        // int 값 (-2^32 ~ 2^31 - 1)을 넘어가는 정수에 대해서는 숫자L 표시 필요!
+        // long c = 10000000000;
+
+        long d = 10000000000L;
+    }
+}
