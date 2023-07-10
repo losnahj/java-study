671d89dcc40d34aee18e111c6570c30a623f5422
DESKTOP-CSH3D9L\My <zmfpdlwm130@naver.com>
2023-07-10 22:58:24 +0900

    27강) 3.2 산술 연산자

--- src/ch03/sec01/SignOperatorExample.java
+++ src/ch03/sec01/SignOperatorExample.java
@@ -1,13 +1,17 @@
-package ch03;
+package ch03.sec01;
 
 public class SignOperatorExample {
     public static void main(String[] args) {
-        byte a = 100;
+        byte a = 127;
 
         // - 또는 + 는 연산자 이다.
-        // 즉, byte, short, char, int 4가지 타입에 대해서 연산 결과는 무조건 int 이다.
+        // 즉, byte, short, int 3가지 타입에 대해서 연산 결과는 무조건 int 이다.
         // 따라서, byte 타입 변수의 - 연산 결과는 int 이기 때문에 컴파일 에러가 발생한다.
         //byte b = -a;
         int c = -a;
+        ++a;
+        byte t = ++a;
+        System.out.println(t);
+        int d = a++;
     }
 }
