6e21ddcdae738af1edec1adad3fbbb3db2f8c767
DESKTOP-CSH3D9L\My <zmfpdlwm130@naver.com>
2023-07-11 22:43:56 +0900

    26강) 3.4 정확한 계산은 정수 연산

---
+++ src/ch03/sec04/AccuracyExample.java
@@ -0,0 +1,15 @@
+package ch03.sec04;
+
+public class AccuracyExample {
+    public static void main(String[] args) {
+
+        int a = 1;
+
+        double b = 0.1;
+        int c = 7;
+
+        double result = a - b * c;
+
+        System.out.println(result);
+    }
+}
