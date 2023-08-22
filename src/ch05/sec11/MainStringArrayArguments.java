3eeee7b3c5705e4b60840a010c27b65141861dd8
DESKTOP-CSH3D9L\My <zmfpdlwm130@naver.com>
2023-08-22 23:30:43 +0900

    56강) 5.11 main 메서드의 String args

---
+++ src/ch05/sec11/MainStringArrayArguments.java
@@ -0,0 +1,10 @@
+package ch05.sec11;
+
+public class MainStringArrayArguments {
+    public static void main(String[] args) {
+
+        if (args.length != 2) {
+            System.out.println("덧셈을 위해선 두 개의 인자가 필요합니다.");
+        }
+    }
+}
